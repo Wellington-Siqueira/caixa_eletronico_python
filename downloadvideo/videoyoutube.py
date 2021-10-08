00f2001d93f844841a82cd466a467d9901dd45c5
Wellington <wellingtonsiqueira333@gmail.com>
2021-10-08 19:50:01 -0300

    update

--- downloadvideo/videoyoutube.py
+++ downloadvideo/videoyoutube.py
@@ -5,4 +5,4 @@ video = YouTube("https://www.youtube.com/watch?v=9FiEji_fzvk")
 print(video.streams.filter(file_extension="mp4", progressive=True))
 
 video = video.streams.get_by_itag(22)
-video.download(filename="teste.mp4")
+video.download(filename="video.mp4")
