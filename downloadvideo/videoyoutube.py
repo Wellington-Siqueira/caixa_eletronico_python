12cbfa4a2891f119958b3a8343b50d86cfc31ba5
Wellington <wellingtonsiqueira333@gmail.com>
2021-10-08 20:15:46 -0300

    update

--- downloadvideo/videoyoutube.py
+++ downloadvideo/videoyoutube.py
@@ -1,8 +1,14 @@
+#Download de video usando a biblioteca pytube
 from pytube import YouTube
 
-video = YouTube("https://www.youtube.com/watch?v=9FiEji_fzvk")
+#Link do video na qual deseja baixar
+video = YouTube("https://www.youtube.com/watch?v=R4Ukl9u9_4Q") 
 
-print(video.streams.filter(file_extension="mp4", progressive=True))
+#Utilizado para ver todo o conteúdo baixado e também para pegar a tag de download, recomendo ler a documentação do pytube e ir realizando testes
+print(video.streams.filter(file_extension="mp4", progressive=True)) 
 
+#Seleciona o video pela tag obtida pelo comando acima
 video = video.streams.get_by_itag(22)
+
+#realiza o download do video
 video.download(filename="video.mp4")
